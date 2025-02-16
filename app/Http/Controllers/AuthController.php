3dfad24f5c21f3a07cfaa5f05e4b726dc01fcc90
MATT DANIELLE BRAVO <2233368@slu.edu.ph>
2025-02-16 15:36:35 +0800

    Login and Registration

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login() {
+        return view('auth.login');
+    }
+
+    function loginPost(Request $request) {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended(route("home"));
+        }
+        return redirect("login")->with("error", "Login Failed");
+    }
+
+    function register() {
+        return view('auth.register');
+    }
+
+
+    function registerPost(Request $request) {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = Hash::make($request->input('password'));
+
+        if ($user->save()) {
+            return redirect(route('login'))->with('success', 'Registration Successful Please Login');
+        }
+        return redirect(route('register'))->with('error', 'Registration Failed');
+    }
+}
